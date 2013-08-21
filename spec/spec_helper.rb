be31de23599344f486543d092e344eddab666478
Kevin <kevin.melchert@gmail.com>
2013-08-21 20:28:47 +0200

    add minitest

---
+++ spec/spec_helper.rb
@@ -0,0 +1,8 @@
+# -*- encoding: utf-8 -*-
+require 'rubygems'
+require 'bundler/setup'
+
+require 'minitest/autorun'
+require 'minitest/spec'
+begin; require 'turn/autorun'; rescue LoadError; end
+require 'iban'
